383551b0cf1dfff0e4f2156751be2107ca12cb2e
gokul62 <gokul6262lap@gmail.com>
2025-02-19 18:47:06 +0530

    implication operator in constraint .sv

---
+++ implication operator in constraint .sv
@@ -0,0 +1,23 @@
+class packet 
+
+  rand bit [31:0] value ;
+  rand enum {low,hign } scale ;
+
+  constraint packet1 { (scale == high ) -> value > 78;}
+
+endclass 
+
+module implication_constraint ();
+  packet pkt ;
+  initial begin 
+    pkt= new();
+
+    repeat (3) begin 
+      pkt.randomize();
+      $display (" scale =%s ,value = %d ", pkt.scale,pkt.value );
+    end 
+
+  end 
+  endmodule 
+
+  
